d6977dbde503e895eb8a837897cb287cd35575d1
Abhi_G <devabhi.code@gmail.com>
2019-01-12 02:49:59 +0000

    Broadcasting Works. Messages displayed in multiple client windows upon connection.

--- src/App.jsx
+++ src/App.jsx
@@ -4,37 +4,79 @@ import Navbar from './navbar.jsx';
 import Chatbar from './chatbar.jsx';
 import MessageList from './messageList.jsx';
 
+const uuidv4 = require('uuid/v4');
 
 
 class App extends Component {
   constructor(props) {
     super(props);
 
+    this.socket = new WebSocket('ws://localhost:3001');
+
     this.state = {
       currentUser: {name: 'Bob'},
       
-      messages: [
-                  {
-                    id:"101",
-                    content: "hello",
-                    username: "Bob"
-                  },
-                  { 
-                    id: "102",
-                    content:"Yello",
-                    username: "Tina"
-                  }
-      ]
+      messages: []
     }
+
+    this.addMessage = this.addMessage.bind(this);
+    this.getRandomId = this.getRandomId.bind(this);
+    // this.socket.onmessage = this.socket.onmessage.bind(this);
+  }
+
+  getRandomId() {
+    return Math.floor(Math.random() * Math.floor(100000000000000));
+  }
+
+  addMessage(contentPassedFromChatbar) {
+    const newMessage = {
+      id: uuidv4(),
+      content: contentPassedFromChatbar,
+      username: this.state.currentUser.name
+    }
+
+    this.socket.send(JSON.stringify(newMessage));
+    const newMessages = this.state.messages.concat(newMessage);
+    this.setState(
+      {messages: newMessages}
+    );
+    
   }
 
   componentDidMount() {
     console.log("componentDidMount <App />");
+
+    //Print connected to server when socket server starts
+    this.socket.onopen = () => {
+      console.log('Connected to Socket server');
+
+      this.socket.onmessage = (event) => {
+        console.log(event);
+        const incomingMessage = JSON.parse(event.data);
+        
+        // console.log("BIG EVENT:  ", incomingMessage);
+        // // code to handle incoming message
+
+        const newMessages = this.state.messages.concat(incomingMessage);
+        console.log(newMessages);
+        this.setState(
+          { messages: newMessages }
+        );
+      }
+
+      // this.socket.send("""Hellooooooo1");
+
+
+    }
+
+    
+
+
     setTimeout(() => {
       console.log("Simulating incoming message");
 
       // Add a new message to the list of messages in the data store
-      const newMessage = { id: 3, username: "Michelle", content: "Hello there!" };
+      const newMessage = { id: uuidv4(), username: "Michelle", content: "Hello there!" };
       const messages = this.state.messages.concat(newMessage);
       
       // Update the state of the app component.
@@ -42,6 +84,8 @@ class App extends Component {
       this.setState({ messages: messages })
     }, 3000);
   }
+
+
 
 
   render() {
@@ -52,7 +96,7 @@ class App extends Component {
       <div>
         <Navbar />
         <MessageList messages={this.state.messages}/>
-        <Chatbar currentUser={this.state.currentUser}/>
+        <Chatbar currentUser={this.state.currentUser} addMessage={this.addMessage}/>
       </div>
     );
   }
--- src/chatbar.jsx
+++ src/chatbar.jsx
@@ -8,13 +8,24 @@ class Chatbar extends Component {
       username: "",
       content: ''
     }
+
+    this.handleContentSubmission = this.handleContentSubmission.bind(this);
+
+  }
+
+  handleContentSubmission(event) {
+    if (event.key === 'Enter') {
+      console.log(':Chatbar: event target:', event.target.value)
+      this.props.addMessage(event.target.value);
+
+    }
   }
   render() {
 
     return (
       <footer className="chatbar">
       <input className="chatbar-username" placeholder="Your name(Optional)" defaultValue={this.props.currentUser.name} />
-      <input className="chatbar-message" placeholder="Type and hit Enter!"/>
+      <input className="chatbar-message" onKeyUp={this.handleContentSubmission} placeholder="Type and hit Enter!" />
       </footer>
     );
   }
